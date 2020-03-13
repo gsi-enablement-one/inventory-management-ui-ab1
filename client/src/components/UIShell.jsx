8cd320d78906630964d30e4570924be07e89051d
abalasu1 <abalasu1@in.ibm.com>
2020-03-13 14:14:13 +0530

    Initial shell components

--- client/src/components/UIShell.jsx
+++ client/src/components/UIShell.jsx
@@ -24,7 +24,7 @@ const Fade20 = () => (
 
 class UIShell extends Component {
   header = "Stock Trader";
-  menuTitle = "Inventory Management;
+  menuTitle = "Inventory Management";
   menuItems = [
     "Stock Items",
   ];
